a6faed6f7050e1a89f5a785e512211b7b5dc5836
antonprafanto <anton.prafanto@gmail.com>
2022-09-12 03:49:03 +0800

    commit new

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Data\Person;
+use App\Data\Foo;
+use App\Data\Bar;
 
 class ServiceContainerTest extends TestCase
 {
@@ -55,5 +57,38 @@ class ServiceContainerTest extends TestCase
         self::assertEquals("Anton", $person2->firstName);
         self::assertSame($person1, $person2);
     }
+
+    public function testDependencyInjection1(){
+        $this->app->singleton(Foo::class, function ($app){
+            return new Foo();
+        });
+
+        $foo = $this->app->make(Foo::class);
+        $bar = $this->app->make(Bar::class);
+
+        self::assertEquals("Foo and Bar", $bar->bar());
+        self::assertSame($foo, $bar->foo);
+    }
+
+    public function testDependencyInjection2(){
+        $this->app->singleton(Foo::class, function ($app){
+            return new Foo();
+        });
+
+        $this->app->singleton(Bar::class, function ($app){
+            $foo = $app->make(Foo::class);
+            return new Bar($foo);
+        });
+
+        $foo = $this->app->make(Foo::class);
+        $bar1 = $this->app->make(Bar::class);
+        $bar2 = $this->app->make(Bar::class);
+
+        self::assertEquals("Foo and Bar", $bar1->bar());
+        self::assertSame($foo, $bar1->foo);
+        self::assertSame($bar1, $bar2);
+    }
+
+
 
 }
