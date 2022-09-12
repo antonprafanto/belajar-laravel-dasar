0991bbb37dd9fbe41d94efb978faaa52e3c075c6
antonprafanto <anton.prafanto@gmail.com>
2022-09-12 11:13:30 +0800

    new

---
+++ app/Providers/FooBarServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Data\Foo;
+use App\Data\Bar;
+
+class FooBarServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Foo::class , function () {
+            return new Foo();
+        });
+        $this->app->singleton(Bar::class , function ($app) {
+            return new Bar($app->make(Foo::class));
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    //
+    }
+}
